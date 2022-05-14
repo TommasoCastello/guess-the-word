f12171a7d92734981ccce2fbc4d6824300f76bd8
JanRezzonico <jan.rezzonico@samtrevano.ch>
2022-05-14 14:46:34 +0200

    Room&chat fix, nav bar, user list, leave and destroy

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ io.on('connection', (socket, roomId) => {
       }else{
         socket.emit('alert', "welcome " + playerName + "!");
         roomMap[roomId].players.push(newplayer);
-        socket.join(roomId);
+        socket.join(String(roomId));
         socket.emit('room joined', roomId);
+        console.log(io.sockets.adapter.rooms);
+        io.in(String(roomId)).emit('player updated', roomMap[roomId].players);
       }
     }else{
       socket.emit('alert', "room not found");
@@ -54,17 +56,43 @@ io.on('connection', (socket, roomId) => {
       roomId1++;
     }
     let owner = new Player(playerName);
-    let newRoom = new Room(roomId1, owner);
+    let newRoom = new Room(String(roomId1), owner);
     console.log(owner.name + " created room " + roomId1);
     roomMap[roomId1] = newRoom;
 
     // owner joins new room
     socket.emit('created room', roomId1);
   });
-  socket.on('chat message', (msg, roomId1) => {
-    io.in(roomId1).emit('chat message', msg);
+  socket.on('chat message', (msg, roomId1, username) => {
+    console.log("socket => " + username + ": " + msg);
+    io.in(String(roomId1)).emit('chat message', (username + ": " + msg));
   });
-
+  socket.on('leave room', (room, user) => {
+    console.log(user + " disconnected from room " + room);
+    if(room in roomMap){
+      if(user in roomMap[room].players){
+        roomMap[room].players.splice(roomMap[room].players.indexOf(user), 1);
+        io.in(roomId).emit('player updated', roomMap[roomId].players);
+      }
+    }
+  });
+  socket.on('destroy room', (room, user) => {
+    console.log(roomMap[room].players);
+    if(!(room in roomMap)){
+      socket.emit('alert', "room not found");
+      return;
+    }
+    if(!(roomMap[room].players.some(player => player.name == user))){
+      socket.emit('alert', "you are not in this room");
+      return;
+    }
+    if(roomMap[room].owner.name != user){
+      socket.emit('alert', "you are not the owner of this room");
+      return;
+    }
+    io.in(room).emit('room destroyed');
+    delete roomMap[room];//idk if this works, copilot did it
+  });
 });
 
 function nameAlreadyInUse(playerArray, name){
